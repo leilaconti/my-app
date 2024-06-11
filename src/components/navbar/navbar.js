ab2b1dd09485bd825651bd034fb49508049ca9ea
leila.conti <leilaconti@outlook.com>
2024-06-11 10:03:54 +0100

    navbar component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+import './navbar.css';
 
 export const NavBar = () => {
     return (
-        <div>
-            <p>Home</p>
-            <p>About</p>
-            <p>Shop</p>
-            <p>Contact</p>
+        <div className="navbar">
+            <img src="https://my-first-shop.s3.eu-north-1.amazonaws.com/logo.png" />
+            <div className="navlinks">
+                <p>Home</p>
+                <p>About</p>
+                <p>Shop</p>
+                <p>Contact</p>
+            </div>
         </div>
     )
 }
